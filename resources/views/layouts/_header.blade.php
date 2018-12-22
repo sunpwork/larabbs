77de54126be8add0c0d0c1a9c53ffa4712dae071
vagrant <vagrant@homestead>
2018-12-22 12:33:50 +0000

    user can create topic

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -21,11 +21,16 @@
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             {{--左侧导航栏--}}
             <ul class="nav navbar-nav">
-                <li class="{{ active_class(if_route('topics.index')) }}"><a href="{{ route('topics.index') }}">话题</a></li>
-                <li class="{{ active_class(if_route('categories.show' && if_route_param('category',1))) }}"><a href="{{ route('categories.show', 1) }}">分享</a></li>
-                <li class="{{ active_class(if_route('categories.show' && if_route_param('category',2))) }}"><a href="{{ route('categories.show', 2) }}">教程</a></li>
-                <li class="{{ active_class(if_route('categories.show' && if_route_param('category',3))) }}"><a href="{{ route('categories.show', 3) }}">问答</a></li>
-                <li class="{{ active_class(if_route('categories.show' && if_route_param('category',4))) }}"><a href="{{ route('categories.show', 4) }}">公告</a></li>
+                <li class="{{ active_class(if_route('topics.index')) }}"><a href="{{ route('topics.index') }}">话题</a>
+                </li>
+                <li class="{{ active_class(if_route('categories.show' && if_route_param('category',1))) }}"><a
+                            href="{{ route('categories.show', 1) }}">分享</a></li>
+                <li class="{{ active_class(if_route('categories.show' && if_route_param('category',2))) }}"><a
+                            href="{{ route('categories.show', 2) }}">教程</a></li>
+                <li class="{{ active_class(if_route('categories.show' && if_route_param('category',3))) }}"><a
+                            href="{{ route('categories.show', 3) }}">问答</a></li>
+                <li class="{{ active_class(if_route('categories.show' && if_route_param('category',4))) }}"><a
+                            href="{{ route('categories.show', 4) }}">公告</a></li>
             </ul>
             {{--右侧导航栏--}}
             <ul class="nav navbar-nav navbar-right">
@@ -33,6 +38,13 @@
                     <li><a href="{{ route('login') }}">登录</a></li>
                     <li><a href="{{ route('register') }}">注册</a></li>
                 @else
+
+                    <li>
+                        <a href="{{ route('topics.create') }}">
+                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                        </a>
+                    </li>
+
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
