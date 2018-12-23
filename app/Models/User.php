20c209b394f880e3731f4552b8804c80a69f6bb4
vagrant <vagrant@homestead>
2018-12-23 07:56:49 +0000

    auther can delete topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Topic::class,'user_id',$this->primaryKey);
     }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
