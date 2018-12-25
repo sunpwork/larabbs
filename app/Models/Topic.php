14bf9c864406d86cc41e2d80d15ed58af0b979a3
vagrant <vagrant@homestead>
2018-12-25 06:46:22 +0000

    auto translate slug

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,22 +4,21 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'category_id',  'excerpt', 'slug'];
+    protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
 
     public function category()
     {
-        return $this->belongsTo(Category::class,'category_id','id');
+        return $this->belongsTo(Category::class, 'category_id', 'id');
     }
 
     public function user()
     {
-        return $this->belongsTo(User::class,'user_id','id');
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
-    public function scopeWithOrder($query,$order)
+    public function scopeWithOrder($query, $order)
     {
-        switch ($order)
-        {
+        switch ($order) {
             case 'recent':
                 $query->recent();
                 break;
@@ -29,7 +28,7 @@ class Topic extends Model
                 break;
         }
 
-        return $query->with('user','category');
+        return $query->with('user', 'category');
     }
 
 //    使用本地作用域
@@ -40,11 +39,16 @@ class Topic extends Model
 
     public function scopeRecentReplied($query)
     {
-        return $query->orderBy('updated_at','desc');
+        return $query->orderBy('updated_at', 'desc');
     }
 
     public function scopeRecent($query)
     {
-        return $query->orderBy('created_at','desc');
+        return $query->orderBy('created_at', 'desc');
+    }
+
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
 }
--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Handlers\SlugTranslateHandler;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -21,8 +22,12 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
-        $topic->body = clean($topic->body,'user_topic_body');
+        $topic->body = clean($topic->body, 'user_topic_body');
 
         $topic->excerpt = make_excerpt($topic->body);
+
+        if (!$topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
