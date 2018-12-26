6b6bab46860bacb1d8ad7f3dfa76eaff394342ad
vagrant <vagrant@homestead>
2018-12-26 07:40:42 +0000

    user can delete reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -13,14 +13,19 @@ class ReplyObserver
     public function created(Reply $reply)
     {
         $topic = $reply->topic;
-        $topic->increment('reply_count',1);
+        $topic->increment('reply_count', 1);
 
         $topic->user->notify(new TopicReplied($reply));
     }
 
     public function creating(Reply $reply)
     {
-        $reply->content = clean($reply->content,'user_topic_body');
+        $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->decrement('reply_count', 1);
     }
 
 }
