30e3b55604f5c76364e217ec244dd442f6adb940
vagrant <vagrant@homestead>
2019-03-05 14:34:22 +0800

    vertificationCode store

---
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\VerificationCodeRequest;
+use Illuminate\Http\Request;
+use Overtrue\EasySms\EasySms;
+
+class VerificationCodesController extends Controller
+{
+    public function store(VerificationCodeRequest $request, EasySms $easySms)
+    {
+        $phone = $request->phone;
+
+        if (!app()->environment('production')) {
+            $code = random_int(100000, 999999);
+        } else {
+            try {
+                $easySms->send($phone, [
+                    'template' => '104003',
+                    'data' => [$code],
+                ]);
+            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+                $message = $exception->getException('qcloud')->getMessage();
+                return $this->response->errorInternal($message ?: '短信发送异常');
+            }
+        }
+
+
+        $key = 'verificationCode_' . str_random(15);
+        $expiredAt = now()->addMinute(10);
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
+    }
+}
