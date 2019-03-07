beba1d77dad5ca6386e4f7c9d322b9ed63c0649c
vagrant <vagrant@homestead>
2019-03-07 15:07:46 +0800

    replies index

--- routes/api.php
+++ routes/api.php
@@ -57,10 +57,18 @@ $api->version('v1', [
             ->name('api.categories.index');
         $api->get('topics', 'TopicsController@index')
             ->name('api.topics.index');
+        // 某个用户发布的话题
         $api->get('users/{user}/topics', 'TopicsController@userIndex')
             ->name('api.users.topics.index');
+        // 话题详情
         $api->get('topics/{topic}', 'TopicsController@show')
             ->name('api.topics.show');
+        // 话题回复列表
+        $api->get('topics/{topic}/replies', 'RepliesController@index')
+            ->name('api.topics.replies.index');
+        // 某个用户的回复列表
+        $api->get('users/{user}/replies', 'RepliesController@userIndex')
+            ->name('api.users.replies.index');
 
         // 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function (\Dingo\Api\Routing\Router $api) {
