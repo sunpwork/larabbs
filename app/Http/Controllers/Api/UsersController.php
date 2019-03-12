79feef781357982d38de5e25df232766e91d8197
vagrant <vagrant@homestead>
2019-03-12 14:06:23 +0800

    weapp users store

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -7,6 +7,7 @@ use App\Models\Image;
 use App\Models\User;
 use App\Transformers\UserTransformer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -31,6 +32,51 @@ class UsersController extends Controller
         \Cache::forget($request->verification_key);
 
         return $this->response->created();
+    }
+
+    public function weappStore(UserRequest $request)
+    {
+        $verifyData = \Cache::get($request->verification_key);
+
+        if (!$verifyData) {
+            return $this->response->error('验证码已失效', 422);
+        }
+
+        if (!hash_equals($verifyData['code'], $request->verification_code)) {
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        // 获取微信的openid和session_key
+        $miniProgram = \EasyWeChat::miniProgram();
+        $data = $miniProgram->auth->session($request->code);
+
+        if (isset($data['errcode'])) {
+            return $this->response->errorUnauthorized('code 不正确');
+        }
+
+        $user = User::where('weapp_openid', $data['openid'])->first();
+
+        if ($user) {
+            return $this->response->errorForbidden('微信已绑定其他用户，请直接登录');
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'phone' => $verifyData['phone'],
+            'password' => bcrypt($request->password),
+            'weapp_openid' => $data['openid'],
+            'weixin_session_key' => $data['session_key'],
+        ]);
+
+        // 清除验证码缓存
+        \Cache::forget($request->verification_key);
+
+        return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => Auth::guard('api')->factory()->getTTL() * 60
+            ])->setStatusCode(201);
     }
 
     public function me()
